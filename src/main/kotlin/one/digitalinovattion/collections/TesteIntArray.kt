37e6e55377658c6fa1f6dd4d8f50877319f12635
Casa <carlos_henrique.taqua@hotmail.com>
2021-08-04 21:29:52 -0300

    Initial commit

---
+++ src/main/kotlin/one/digitalinovattion/collections/TesteIntArray.kt
@@ -0,0 +1,36 @@
+package one.digitalinovattion.collections
+
+fun main(){
+    val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 9
+    values[4] = 0
+
+    for (valor in values){
+        println(valor)
+
+    }
+
+    println("----------------------------------")
+
+    values.forEach {valor->
+        println(valor)
+    }
+
+    println("----------------------------------")
+
+    for (index in values.indices){
+        println(values[index])
+    }
+
+    println("----------------------------------")
+
+    values.sort()
+    for (valor in values){
+        println(valor)
+    }
+    
+}
